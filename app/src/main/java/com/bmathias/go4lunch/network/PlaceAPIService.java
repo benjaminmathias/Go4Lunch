c56e0603042cf5228599ceac9ecd5b79c7af3983
Benjamin <benmathias@hotmail.fr>
2021-12-15 15:47:29 +0100

    - API key fix

--- app/src/main/java/com/bmathias/go4lunch/network/PlaceAPIService.java
+++ app/src/main/java/com/bmathias/go4lunch/network/PlaceAPIService.java
@@ -1,23 +1,13 @@
 package com.bmathias.go4lunch.network;
 
-import com.bmathias.go4lunch.BuildConfig;
 import com.bmathias.go4lunch.network.model.RestaurantPlaceDetailsResponse.DetailsResultAPI;
 import com.bmathias.go4lunch.network.model.RestaurantPlaceResponse.ResultsAPI;
 
 import io.reactivex.Observable;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface PlaceAPIService {
-
-    // private static final String BASE_URL_SEARCH = "https://maps.googleapis.com/maps/api/place/nearbysearch/";
-    // location=43.80812051168388,4.638306531512217
-    // key = &key=AIzaSyDKVEFMvGvHtXCQeWzF1_xYjVDHLuikiCE
-
-    // place/nearbysearch/json?location=43.80812051168388,4.638306531512217&radius=1000&type=restaurant&key=AIzaSyDKVEFMvGvHtXCQeWzF1_xYjVDHLuikiCE
 
     @GET("place/nearbysearch/json")
     Observable<ResultsAPI> getRestaurantsResult(
